ff679e714fd673a9098754700261f2ac2be006eb
Jarrett Phillips <jphill01@uoguelph.ca>
2021-06-22 22:32:40 -0400

    Add files via upload

--- App/app.R
+++ App/app.R
@@ -33,54 +33,62 @@ ui <- fluidPage(
 
     # Application title
     titlePanel("HACSim: Haplotype Accumulation Curve Simulator"),
-        sidebarLayout(
-            sidebarPanel(
-                numericInput(inputId = "perms",
-                             label = "Number of permutations (perms)",
-                             value = 10000,
-                             min = 10,
-                             max = 10000,
-                             step = 10),
+    tabsetPanel(
+        tabPanel("Main algorithm parameters",
+            sidebarLayout(
+                sidebarPanel(
+                    numericInput(inputId = "perms",
+                                 label = "Number of permutations (perms)",
+                                 value = 10000,
+                                 min = 2,
+                                 step = 1),
                 
-                numericInput(inputId = "p",
-                             label = "Proportion of haplotypes to recover (p)",
-                             value = 0.95,
-                             min = 0,
-                             max = 1,
-                             step = 0.01), 
+                    numericInput(inputId = "p",
+                                 label = "Proportion of haplotypes to recover (p)",
+                                 value = 0.95,
+                                 min = 0,
+                                 max = 1,
+                                 step = 0.01), 
         
-                numericInput(inputId = "conf.level",
-                             label = "Confidence level for calculations and plotting (conf.level)",
-                             value = 0.95,
-                             min = 0.01,
-                             max = 0.99,
-                             step = 0.01)
+                    numericInput(inputId = "conf.level",
+                                 label = "Confidence level for calculations and plotting (conf.level)",
+                                 value = 0.95,
+                                 min = 0.01,
+                                 max = 0.99,
+                                 step = 0.01),
                 
-                    ), # end sidebarPanel
+              
+                
+                        ), # end sidebarPanel
             
-                mainPanel(
+                    mainPanel(
                     
-                )
+                    )
                    
-            ), # end sidebarLayout
+                ), # end sidebarLayout
+            
+            actionButton(inputId = "submit", 
+                         label = "Submit"),
+            
+        ), # end tabPanel
 
-            tabsetPanel(
+            br(),
                 tabPanel("Hypothetical species", 
-                         sidebarLayout(
-                             sidebarPanel(
-                                 numericInput(inputId = "N",
-                                              label = "Number of observed specimens (N)",
-                                              value = 100,
-                                              min = 2),
+                    sidebarLayout(
+                        sidebarPanel(
+                            numericInput(inputId = "N",
+                                         label = "Number of observed specimens (N)",
+                                         value = 100,
+                                         min = 2),
                                  
-                                 numericInput(inputId = "Hstar",
-                                              label = "Number of observed haplotypes (H*)",
-                                              value = 5,
-                                              min = 1),
+                            numericInput(inputId = "Hstar",
+                                         label = "Number of observed haplotypes (H*)",
+                                         value = 5,
+                                         min = 1),
                                  
-                                 textInput(inputId = "probs",
-                                           label = "Haplotype frequency distribution",
-                                           value = "0.20, 0.20, 0.20, 0.20, 0.20"),
+                            textInput(inputId = "probs",
+                                      label = "Haplotype frequency distribution",
+                                      value = "0.20, 0.20, 0.20, 0.20, 0.20"),
                                  
                               
                              
@@ -130,12 +138,17 @@ ui <- fluidPage(
                                                                  step = 0.01)
                                                     ),
                          
-                                   actionButton(inputId = "submit", 
-                                                label = "Submit")
                          
-                ) # end tabPanel
+                ), # end tabPanel
+        
+            br(),
+            tabPanel("About",
+                     
+            ) # end tanPanel
+                     
                
-            ) # end tabsetPanel
+            ), # end tabsetPanel
+    
         
 ) # end fluidPage
 
